c4f3844d8e388209ecdae3e5496967070d4e16f8
Sam Dupas <samgd1031@gmail.com>
2022-02-09 23:36:49 -0800

    added dupEngine class
    class will handle communication from GUI and so on
    
    also added a super cool ASCII splash

---
+++ src/dupEngine.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "utils.h"
+
+// class for the DupChess Engine
+// main class to handle UCI commands as well as manage the thinking about chess part
+class DupEngine {
+public:
+	DupEngine(); // constructor
+	
+	static const std::string START_FEN;
+
+	// informational functions
+	std::string getEngineName();
+	std::string getEngineVersion();
+	std::string getAuthorName();
+	std::string getEmailAddress();
+	std::string getCountryName();
+
+	
+};
