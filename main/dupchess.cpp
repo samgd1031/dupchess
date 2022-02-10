e473305f8dea4c4d690e8ceb789b73189ec27e9a
Sam Dupas <samgd1031@gmail.com>
2022-02-09 22:10:14 -0800

    modified dupchess.cpp to echo stdin
    later will add capability to handle commands

--- main/dupchess.cpp
+++ main/dupchess.cpp
@@ -1,33 +1,29 @@
 
 #include <iostream>
 #include <bitset>
+#include <string>
 
 #include "board.h"
 
 
 int main()
 {
-	std::string FEN;
+	std::string cmd_string;
 	Board gameboard = Board();
 
+	std::cout << "Waiting for input..." << std::endl;
+	while (std::getline(std::cin, cmd_string)) {
+		// exit if command string is "exit"
+		if (_stricmp(cmd_string.c_str(), "exit") == 0) {
+			std::cout << "Exiting..." << std::endl;
+			break;
+		}
+		else {
+			std::cout << "cmd: " << cmd_string << std::endl;
+			std::cout << "Waiting for input..." << std::endl;
+		}
+	}
 
-	FEN = "rn2k3/pp3p1p/5b2/3p1b1p/2P5/1P6/PB1P4/2K2q2 w q - 6 28";
-	gameboard.setBoardFromFEN(FEN);
-
-	printf("\nAll Pieces");
-	printbitboard(gameboard.current_state.white_pcs | gameboard.current_state.black_pcs);
-
-	printf("\nWhite Rooks");
-	printbitboard(gameboard.current_state.white_pcs & gameboard.current_state.rooks);
-
-	printf("\nBishops");
-	printbitboard(gameboard.current_state.bishops);
-
-	std::cout << "Castling Rights (KQkq): " << gameboard.current_state.castleRights << std::endl;
-	std::cout << "En Passant Target: " << squareStrings[int(gameboard.current_state.PIPI)] << std::endl;
-	std::cout << "To Move: " << ((gameboard.current_state.whiteToMove) ? "White" : "Black") << std::endl;
-	std::cout << "Half Move Counter: " << gameboard.current_state.halfmove << std::endl;
-	std::cout << "Full Move Counter: " << gameboard.current_state.fullmove << std::endl;
 
 	return 0;
 }
