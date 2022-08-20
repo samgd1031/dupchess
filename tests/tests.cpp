c0e0e6dd40d059b7e1480d2c2e018f98315b884e
Sam Dupas <samgd1031@gmail.com>
2022-08-20 11:08:02 -0700

    fixed setup for gtest
    now unit tests actually work.  Started adding some to check move generation works

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,11 +1,56 @@
 #include <gtest/gtest.h>
+#include "../src/chess/dupEngine.h"
+
+class DupEngineTest : public testing::Test {
+protected:
+	void SetUp() override {
+		eng_ = DupEngine();
+	}
+
+	DupEngine eng_;
+};
 
 
-
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-	// Expect two strings not to be equal.
-	EXPECT_STRNE("hello", "world");
-	// Expect equality.
-	EXPECT_EQ(7 * 6, 42);
+// series of perft tests to check move generation
+TEST_F(DupEngineTest, perft) {
+	// from initial position
+	ASSERT_EQ(eng_.perft(0), 1);
+	ASSERT_EQ(eng_.perft(1), 20);
+	ASSERT_EQ(eng_.perft(2), 400);
+	ASSERT_EQ(eng_.perft(3), 8902);
+	ASSERT_EQ(eng_.perft(4), 197281);
 }
+
+TEST_F(DupEngineTest, illegalEP){
+	/////// edge cases from http://www.talkchess.com/forum3/viewtopic.php?t=47318
+	// avoid illegal en passant capture:
+	eng_.gameboard.setBoardFromFEN("8/5bk1/8/2Pp4/8/1K6/8/8 w - d6 0 1");
+	EXPECT_EQ(eng_.perft(6), 824064);
+	eng_.gameboard.setBoardFromFEN("8/8/1k6/8/2pP4/8/5BK1/8 b - d3 0 1");
+	EXPECT_EQ(eng_.perft(6), 824064);
+}
+/*
+	// en passant capture checks opponent
+	eng.gameboard.setBoardFromFEN("8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1");
+	EXPECT_EQ(eng.perft(6), 1440467);
+	eng.gameboard.setBoardFromFEN("8/5k2/8/2Pp4/2B5/1K6/8/8 w - d6 0 1");
+	EXPECT_EQ(eng.perft(6), 1440467);
+
+	// short castling gives check:
+	eng.gameboard.setBoardFromFEN("5k2/8/8/8/8/8/8/4K2R w K - 0 1");
+	EXPECT_EQ(eng.perft(6), 661072);
+	eng.gameboard.setBoardFromFEN("4k2r/8/8/8/8/8/8/5K2 b k - 0 1");
+	EXPECT_EQ(eng.perft(6), 661072);
+
+	// long castling gives check:
+	eng.gameboard.setBoardFromFEN("3k4/8/8/8/8/8/8/R3K3 w Q - 0 1");
+	EXPECT_EQ(eng.perft(6), 803711);
+	eng.gameboard.setBoardFromFEN("r3k3/8/8/8/8/8/8/3K4 b q - 0 1");
+	EXPECT_EQ(eng.perft(6), 803711);
+
+	// castling (including losing cr due to rook capture):
+	eng.gameboard.setBoardFromFEN("r3k2r/1b4bq/8/8/8/8/7B/R3K2R w KQkq - 0 1");
+	EXPECT_EQ(eng.perft(6), 1274206);
+	eng.gameboard.setBoardFromFEN("r3k2r/7b/8/8/8/8/1B4BQ/R3K2R b KQkq - 0 1");
+	EXPECT_EQ(eng.perft(6), 1274206);
+}*/
