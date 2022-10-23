6e526655de5066d643d91d57f3e27d436fb3116a
Sam Dupas <samgd1031@gmail.com>
2022-10-22 21:32:17 -0700

    modified some unit tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -31,14 +31,17 @@ TEST(MoveGen, EPCheck) {
 	eng.gameboard.setBoardFromFEN("8/5k2/8/2Pp4/2B5/1K6/8/8 w - d6 0 1");
 	EXPECT_EQ(eng.perft(6), 1440467);
 }
-/*
+
+TEST(MoveGen, ShortCastleCheck) {
+	DupEngine eng = DupEngine();
 	// short castling gives check:
 	eng.gameboard.setBoardFromFEN("5k2/8/8/8/8/8/8/4K2R w K - 0 1");
 	EXPECT_EQ(eng.perft(6), 661072);
 	eng.gameboard.setBoardFromFEN("4k2r/8/8/8/8/8/8/5K2 b k - 0 1");
 	EXPECT_EQ(eng.perft(6), 661072);
+}
 
-	// long castling gives check:
+/*	// long castling gives check:
 	eng.gameboard.setBoardFromFEN("3k4/8/8/8/8/8/8/R3K3 w Q - 0 1");
 	EXPECT_EQ(eng.perft(6), 803711);
 	eng.gameboard.setBoardFromFEN("r3k3/8/8/8/8/8/8/3K4 b q - 0 1");
@@ -49,4 +52,6 @@ TEST(MoveGen, EPCheck) {
 	EXPECT_EQ(eng.perft(6), 1274206);
 	eng.gameboard.setBoardFromFEN("r3k2r/7b/8/8/8/8/1B4BQ/R3K2R b KQkq - 0 1");
 	EXPECT_EQ(eng.perft(6), 1274206);
-}*/
+
+}
+*/
